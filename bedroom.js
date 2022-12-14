4b3480f64d2f359737d073af8a2540c9ca84645e
Mahak2009 <88013336+Mahak2009@users.noreply.github.com>
2022-12-13 21:13:58 -0600

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,40 @@
+img = "";
+status1 = "";
+
+function preload()
+{
+    img = loadImage("bed.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects ";
+}
+
+function modelLoaded()
+{
+    console.log("model is loaded");
+    status1 = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    else 
+    {
+        console.log(results);
+    }
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
+
---
+++ fruitbasket.js
@@ -0,0 +1,39 @@
+img = "";
+status1 = "";
+
+function preload()
+{
+    img = loadImage("fruitbasket.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects ";
+}
+
+function modelLoaded()
+{
+    console.log("model is loaded");
+    status1 = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    else 
+    {
+        console.log(results);
+    }
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
